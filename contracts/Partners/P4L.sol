249f7451c4792f3867685ef0534df3a933838277
Daksh Joshi <64137744+dakshjoshi@users.noreply.github.com>
2021-09-21 14:26:20 +0530

    Update P4L.sol
    Quotes will be signed offchain and added onchain sign verifying function.

--- contracts/Partners/P4L.sol
+++ contracts/Partners/P4L.sol
@@ -2,6 +2,7 @@ pragma solidity 0.8.7;
 
 import "../Storage.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /*
 * @dev This contract is accessed by Proxy contract 
@@ -23,7 +24,7 @@ constructor () public {
     initialize(msg.sender);
 }
 
-event insurancBought(address _P4L, address _buyer);
+event insurancBought(bytes32 _partner, address _buyer);
 
 function initialize(address _owner) public {
 owner = _owner;
@@ -33,28 +34,79 @@ owner = _owner;
 // return uintStorage[_variable];
 // }
 
-function buyDeviceInsurance(address proxyAddress,
+//BUSD, BNB, DAI, USDC and CVR
+
+function approve(bytes32 _partner, address _currencyContract, uint _amount) public {
+    require(partners[_partner].currencies[_currencyContract] == true, 'Not a supported currency');
+    IERC20(_currencyContract).approve(_msgSender(), _amount);
+}
+
+function buyDeviceInsurance(bytes32 _partner,
+bytes memory sig,
+uint256 nonce,
 address _buyer,
 string memory _device,
 string memory _company,
 uint256 _amountPaid,
 string memory _currency,
-uint _period) public {
-uint _totalBought = totalBought[_buyer];
+address _currencyContract,
+uint _period) public payable{
 
-    //pd => ProductDetails
-    productDetails storage pd = userBought[_buyer][_totalBought];
-    pd.Product['device'] = _device;
-    pd.Product['company'] = _company;
-    pd.Product['currency'] = _currency;
+//Checking the nonce and increamenting it
+require(users[_msgSender()].IntValues['nonce'] == nonce.add(1),'Invalid Nonce');
+users[_msgSender()].IntValues['nonce'] = users[_msgSender()].IntValues['nonce'].add(1);
 
-pd.details['amount'] = _amountPaid;
-pd.timeStamp = block.timestamp;
-pd.exists = true;
+//Verifying the amount, currency and all details against the message and signer
+bytes32 message = prefixed(keccak256(abi.encodePacked(_msgSender(), _amountPaid, nonce, address(this), _currencyContract, _period)));
 
-totalBought[_buyer] = _totalBought.add(1);
-emit insurancBought(proxyAddress, _buyer);
+//Verifying the signer
+(uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+address signer = ecrecover(message, v, r, s);
+
+require(signer == partners[_partner].addresses['signer'], 'Not a valid signer');
+require(signer == address(0), 'Not a valid signer');
+
+//Transferring the funds
+ require(partners[_partner].currencies[_currencyContract] == true, 'Not a supported currency');
+ IERC20(_currencyContract).transferFrom(_msgSender(), address(this), _amountPaid);
+
+//Storing purchase details
+
+//pd => ProductDetails
+
+productDetails storage pd = everyBought[everyBought.length];
+pd.details['Product']['device'] = _device;
+pd.details['Product']['company'] = _company;
+pd.details['Product']['currency'] = _currency;
+pd.IntValues['paid'] = _amountPaid;
+pd.IntValues['timeStamp'] = block.timestamp;
+
+//Asserting
+assert(users[_msgSender()].IntValues['nonce'] == nonce);
+
+//Declaring that a new purchase has been made
+emit insurancBought(_partner, _buyer);
 
 }
+
+function splitSignature(bytes memory sig)
+    internal
+    pure
+    returns (uint8 v, bytes32 r, bytes32 s) {
+    require(sig.length == 65);
+    assembly {
+    // first 32 bytes, after the length prefix.
+        r := mload(add(sig, 32))
+        // second 32 bytes.
+        s := mload(add(sig, 64))
+        // final byte (first byte of the next 32 bytes).
+        v := byte(0, mload(add(sig, 96)))
+    }
+    return (v, r, s);
+}
+
+function prefixed(bytes32 hash) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
 
 }
