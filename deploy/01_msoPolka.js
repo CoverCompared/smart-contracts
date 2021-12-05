8a48d3a225e0f4a448023f3addb96abc890cf39b
luckywebdev <webdevelop827@gmail.com>
2021-12-06 03:29:08 +0700

    fix: redeploy ExchangeAgent in rinkeby

--- deploy/01_msoPolka.js
+++ deploy/01_msoPolka.js
@@ -8,7 +8,7 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
   const { deployer } = await getNamedAccounts();
 
   const _WETH = '0xc778417e063141139fce010982780140aa0cd5ab'; // UniswapV2 Router
-  const _exchangeAgent = '0x9Bb129C697b9E8593f329253AFB8Bc777B8Cc892';
+  const _exchangeAgent = '0xed698505e496B0c39Eb42B9696f141C5526DdF24';
   const _devWallet = '0x6C641CE6A7216F12d28692f9d8b2BDcdE812eD2b';
   const _multiSigWallet = '0xC31F8d97B0B9fcD8A30F9AA906335dCCdba99bD5';
 
--- deploy/exchangeAgent.js
+++ deploy/exchangeAgent.js
@@ -9,10 +9,14 @@
 /**
  * @note These are rinkeby addresses, should be changed whenever switching network
  */
-const USDC = '0x5FED7f0b36374B8Ee25177b3cbE21B6c8E079F4E';
-const WETH = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'; // Uniswap V2 WETH
+const USDC = '0xD4D5c5D939A173b9c18a6B72eEaffD98ecF8b3F6';
+const WETH = '0xc778417e063141139fce010982780140aa0cd5ab'; // Uniswap V2 WETH
 const UNISWAPV2_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
-const TWAP_PRICE_FEED_FACTORY = '0x08851c9fAa2b1a96097618aD5C7bd6aF781a5ba7';
+const TWAP_PRICE_FEED_FACTORY = '0x6fa8a7E5c13E4094fD4Fa288ba59544791E4c9d3';
+// const USDC = '0x5FED7f0b36374B8Ee25177b3cbE21B6c8E079F4E';
+// const WETH = '0xd0a1e359811322d97991e03f863a0c30c2cf029c'; // Uniswap V2 WETH
+// const UNISWAPV2_FACTORY = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+// const TWAP_PRICE_FEED_FACTORY = '0x08851c9fAa2b1a96097618aD5C7bd6aF781a5ba7';
 module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
