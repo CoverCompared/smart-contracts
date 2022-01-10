fd361ecf934a5a56066e56049a5214fb98762051
snowwhitedev <snowwhite91924@gmail.com>
2022-01-10 22:03:16 +0700

    feat: deploy on testnet again

--- scripts/deploy/rinkeby/01_msoPolka.js
+++ scripts/deploy/rinkeby/01_msoPolka.js
@@ -8,7 +8,7 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
   const { deployer } = await getNamedAccounts();
 
   const _WETH = '0xc778417e063141139fce010982780140aa0cd5ab'; // UniswapV2 Router
-  const _exchangeAgent = '0x9717cA3f6b5E560d244D41791D5760316d2b7bFE';
+  const _exchangeAgent = '0x3320C193109265faC179cC3eF0a466Cca01651DF';
   const _devWallet = '0x6C641CE6A7216F12d28692f9d8b2BDcdE812eD2b';
   const _signer = '0x356C9B814a5D3508ab76834D0eF67e6A171d3740';
 
